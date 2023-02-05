8bbd5e18d5538b053a022afdf0a1a81b484e5905
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-05 13:21:36 +0100

    zastosowano pętle do z możliwością obsługi wyjątków

--- 2ndHomeworkAkademia/Program.cs
+++ 2ndHomeworkAkademia/Program.cs
@@ -1,35 +1,52 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Net.WebSockets;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        //1.Aplikacja losuje 1 liczbę od 0 do 100
-
         var randomizer = new Random();
         var numberToGuess = randomizer.Next(1, 100);
-
-        //2.Użytkownik próbuje odgadnąć liczbę
+        var counter = 0;
 
         Console.WriteLine("Wylosowałem dla Ciebie liczbę.");
         Console.Write("Spróbuj ją odgadnąć: ");
-        var userInput = int.Parse(Console.ReadLine());
-
-        //3. Komunikat jeśli podana liczba jest za mała
-        if (userInput < numberToGuess)
+        int userInput = 0;
+        do
         {
-            Console.WriteLine("Niestety, podana przez Ciebie liczba jest za mała.");
-        }
-        //4. Komunikat jeśli podana liczba jest za duża
-        else if (userInput > numberToGuess) 
-        { 
-            Console.WriteLine("Niestety, podana przez Ciebie liczba jest za duża."); 
-        }
-        //5. Komunikat po odgadnięciu liczby -  odgadłeś wylosowaną liczbę w x próbach
-        else if (userInput== numberToGuess) 
-        {
-            Console.WriteLine("Udało Ci się!");
-            Console.WriteLine($"Odgadłeś wylosowaną liczbę w /counter/ próbach");
-        }
+            try
+            {
+                if (!int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    throw new Exception("Podana została nieprawidłowa liczba.");
+                }
+                else
+                {
+                    if (userInput < numberToGuess)
+                    {
+                        Console.WriteLine("Niestety, podana przez Ciebie liczba jest za mała.");
+                        Console.Write("Spróbuj ponownie: ");
+                    }
+                    else if (userInput > numberToGuess)
+                    {
+                        Console.WriteLine("Niestety, podana przez Ciebie liczba jest za duża.");
+                        Console.Write("Spróbuj ponownie: ");
+                    }
+                    else if (userInput == numberToGuess)
+                    {
+                        Console.WriteLine("\nUdało Ci się!");
+                        Console.WriteLine($"Odgadłeś wylosowaną liczbę w {counter} próbach");
+                    }
+                    counter++;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Podana została nieprawidłowa liczba.");
+                continue;
+            }
+            
+        } 
+        while (userInput != numberToGuess);
     }
 }
